ee124fa59d33abbc70bc9cbac42e71a409899c57
Dedenis <gaeldedenis01@laposte.net>
2020-06-22 11:08:34 +0200

    Création de la fonction d'appel AJAX

--- Sources/ajax.js
+++ Sources/ajax.js
@@ -1,3 +1,26 @@
-function ajaxGet () {
-        
+"use strict";
+
+function newCall(url, callback) {
+
+    var request = new XMLHttpRequest();
+
+    request.open("GET", url);
+
+    // Control de l'avancement de la requette
+    request.onreadystatechange = function() {
+        console.log("Avancement de la requête : " + request.readyState + " /4");
+    }
+
+    // Détection Fin de l'appel
+    request.addEventListener("load", function() {
+        if (request.status >= 200 && request.status < 400) {
+            console.log("Toutes les ressources sont chargées !");
+            let jsonResponse = JSON.parse(this.response);
+            callback(jsonResponse);
+        } else {
+            console.error(request.status + " " + request.statusText + " " + url);
+        }
+    });
+
+    req.send(null);
 }
