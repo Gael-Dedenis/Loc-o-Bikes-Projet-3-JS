6c71974d88ea020d36aae8b7c9c257f6862eb8c2
Dedenis <gaeldedenis01@laposte.net>
2020-07-13 10:25:44 +0200

    Correction sélection par ID et non Query

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     // +++++ +++++ +++++ Carousel +++++ +++++ +++++
-    let carousel = new Carousel(document.querySelector("#carousel--container"), {
+    let carousel = new Carousel(document.getElementById("carousel--container"), {
         slidesToScroll: 4,
         slideAuto: true 
     });
 
     // +++++ +++++ +++++ Map +++++ +++++ +++++
-    let map = new Map(document.querySelector("#leaflet--map"), 15, {
+    let map = new Map(document.getElementById("leaflet--map"), 15, {
         lat: 45.750000,
         long: 4.850000
     });
