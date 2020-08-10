db209db58ffcfd55e9c83819049993756d54816c
Dedenis <gaeldedenis01@laposte.net>
2020-08-10 10:12:49 +0200

    Ajout prise en charge Canvas sur mobile

--- js/Canvas.js
+++ js/Canvas.js
@@ -2,10 +2,11 @@
 
 class Canvas {
 
-    constructor (container, canvascontext) {
-        this.container = container;
-        this.ctxCanvas = canvascontext;
+    constructor (container, ctxCanvas) {
+        this.container    = container;
+        this.ctxCanvas    = ctxCanvas;
         this.startDrawing = false;
+        this.sign         = false;
 
         this.initCanvas();
     }
@@ -15,30 +16,61 @@ class Canvas {
         this.getCanvasContext();
         this.setPointer();
         this.listenCanvasEventsMouse();
+        this.listenCanvasEventsTouch();
     }
 
-    // Events Canvas
+    // Events Canvas Souris
     listenCanvasEventsMouse() {
-        //Au moment du clic sur la souris
         this.container.addEventListener("mousedown", (evnt) => {
             this.startDrawing = true;
             [this.X, this.Y] = [evnt.offsetX, evnt.offsetY];
-
-            //Pendant le déplacement de la souris avec le clic enfoncer
-            this.container.addEventListener("mousemove", (evnt) => {
-                this.paintInCanvas(evnt.offsetX, evnt.offsetY);
-            });
         });
 
-        //Au moment ou l'on relache le clic de la souris
+        this.container.addEventListener("mousemove", (evnt) => {
+            this.paintInCanvas(evnt.offsetX, evnt.offsetY);
+        });
+
         this.container.addEventListener("mouseup", () => {
             this.startDrawing = false;
+            this.sign         = true;
+            sessionStorage.setItem("sign", this.sign);
         });
 
-        //Si la souris sort du champ
         this.container.addEventListener("mouseout", () => {
             this.startDrawing = false;
         });
+    }
+
+    // Events Canvas Mobile
+    listenCanvasEventsTouch() {
+
+        this.container.addEventListener("touchstart", (evnt) => {
+            this.startDrawing = true;
+            this.getTouchPosition(evnt);
+            [this.X, this.Y] = [this.touchX, this.touchY];
+        });
+
+        this.container.addEventListener("touchmove", (evnt) => {
+            evnt.preventDefault();
+            this.paintInCanvas(this.touchX, this.touchY);
+        });
+
+        this.container.addEventListener("touchend", () => {
+            this.startDrawing = false;
+            this.sign         = true;
+            sessionStorage.setItem("sign", this.sign);
+        });
+
+        this.container.addEventListener("touchOut", () => {
+            this.startDrawing = false;
+        });
+    }
+
+    getTouchPosition(evnt) {
+        let canvasAttributes = evnt.target.getBoundingClientRect();
+
+        this.touchX = evnt.targetTouches[0].clientX - canvasAttributes.left;
+        this.touchY = evnt.targetTouches[0].clientY - canvasAttributes.top;
     }
 
     // methode get context
@@ -57,7 +89,6 @@ class Canvas {
         if (!this.startDrawing) {
             return;
         }
-
         this.ctx.strokeStyle = "#000000";
         this.ctx.beginPath();
         this.ctx.moveTo(this.X, this.Y);
